f4ce317cc6462ba16c86c9491335fc442a378cf1
VinhHien <vinhhien317998@gmail.com>
2018-12-03 22:56:40 +0700

    Hien-3/12/2018

---
+++ app/src/main/java/hoxucompany/thoikhoabieu/DayActivity.java
@@ -0,0 +1,17 @@
+package hoxucompany.thoikhoabieu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Admin on 12/03/2018.
+ */
+
+public class DayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day);
+    }
+}
